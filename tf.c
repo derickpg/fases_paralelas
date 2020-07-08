c80d7908242bfba22266899374153af94ad9e0fb
ViniciusAz <viniciussazevedo@gmail.com>
2020-07-08 17:06:43 -0300

    ate maior ok?

--- tf.c
+++ tf.c
@@ -4,26 +4,25 @@
 
 
 main(int argc, char** argv) {
-    
-    printf("♥♥♥ LA NO COMECAO! \n");
-    
+        
     int tam_vet = 10000;   /* Tamanho TOTAL do VETOR  ---- Multiplo da quantidade de processos <- LEMBRAR ! */
+    int debug = 1;
 
 
     int my_rank;        /* Identificador do processo */
     int np;             /* Número de processos */
     int source;         /* Identificador do proc.origem */
     int dest;           /* Identificador do proc. destino */
-    int *vetor;
+    int* vetor;
     int i;
     double ti,tf;
     int tam_part;       /* Tamanho das Partes do vetor */
     int ini_vetor;
     int fim_vetor;
-    int ordenado = 0;   /* 0 = nao ordenado  - 1 = ordenado */
     int recebido;
-    int pronto = 0; 
+    int pronto = 0; // 0 = false e 1 = true
     int flag_erro = 0; /* 0 - sem erros ; 1 - com erros (Flag que indica que se o processo teve um erro de ordenação com o vizinho )*/
+    int primeira_vez = 0;
     MPI_Status status;  /* Status de retorno */
 
     /* TAGS */
@@ -32,6 +31,7 @@ main(int argc, char** argv) {
     int tag_ordenado    = 3;
     int tag_feedback    = 4;
     int tag_erro        = 5;
+    int tag_broadcast   = 6;
     int tag_fim         = 9;
 
     MPI_Init(&argc, &argv);
@@ -40,36 +40,159 @@ main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &np);
 
     tam_part = tam_vet/np; //cada array eh fragmentado em TamanhoArray / numProcessos
+    vetor = malloc(sizeof *vetor * tam_vet);
 
-    printf("♥♥♥ aNTES DO MY 0 \n");
-    if (my_rank == 0){  
-        printf("Comecando a ordenacao! \n");
-        vetor = malloc(tam_vet*sizeof(int));
+    if (my_rank == 0){
 
         for (i=0 ; i<tam_vet; i++)
             vetor[i] = tam_vet-i;
 
-        printf("♥♥♥ aNTES DO broadcast \n");
-
         //https://mpitutorial.com/tutorials/mpi-broadcast-and-collective-communication/
-        //exemplo: MPI_Bcast(data, num_elements, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(vetor, tam_vet, MPI_INT, 0, MPI_COMM_WORLD);
+        //MPI_Bcast(vetor, tam_vet, MPI_INT, 0, MPI_COMM_WORLD);
 
-        printf("♥♥♥ depois DO broadcast \n");
+        for (i = 1; i < np; i++)
+            MPI_Send(vetor,tam_vet,MPI_INT,i,tag_broadcast,MPI_COMM_WORLD);
+
         // Começa a conta o tempo
         ti = MPI_Wtime();
     }
 
-    //if(my_rank != 0)
-    //    MPI_Recv(vetor, tam_vet, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-
+    if(my_rank != 0)
+        MPI_Recv(vetor, tam_vet, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
     fim_vetor = ((my_rank + 1) * tam_part) -1;
     ini_vetor = (fim_vetor+1) - tam_part;
 
     MPI_Barrier(MPI_COMM_WORLD);
+    if(debug == 1) printf("♥♥♥ pid = %d esta com array completo, seus limites sao ini= %d e fim= %d, mas pode ver todo, exemplo? 5001 = %d,\n", my_rank, ini_vetor, fim_vetor, vetor[5001]);
 
-    printf("♥♥♥ pid = %d recebeu array, ini= %d  e fim= %d \n", my_rank, ini_vetor, fim_vetor);
+    pronto = 0;
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    while (pronto == 0) {
+        //na primeira rodada ninguem recebe nada.
+        if(primeira_vez != 0)
+            MPI_Recv(&recebido, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+        if((status.MPI_TAG == tag_inicio) || (status.MPI_TAG == tag_erro) || (flag_erro == 1) || primeira_vez == 0){
+            if(debug == 1) printf("♥♥♥ DEBUG [ %d ] TAG_INICIO \n", my_rank);
+            
+            primeira_vez = 1;
+
+            // ordeno vetor local
+            for (i = ini_vetor; i <= fim_vetor; i++) {
+                int i_bs, troca;
+                for (i_bs = i+1; i_bs <= fim_vetor; i_bs++) {
+                    if (vetor[i] > vetor[i_bs]) {
+                        troca       = vetor[i];
+                        vetor[i]    = vetor[i_bs];
+                        vetor[i_bs] = troca;
+                    }
+                }
+            }
+            // verifico condição de parada ???
+
+            // se não for np-1, mando o meu maior elemento para a direita
+            if(my_rank < (np-1)) // Manda o último número...
+                MPI_Send(&vetor[fim_vetor],1,MPI_INT,(my_rank+1),tag_maior,MPI_COMM_WORLD);
+            if(debug == 1) printf("♥♥♥ DEBUG [ %d ] TAG_INICIO-fim \n", my_rank);
+
+        } else if(status.MPI_TAG == tag_maior) {
+            if(debug == 1) printf("♥♥♥ DEBUG [ %d ] TAG_MAIOR \n", my_rank);
+            // se não for 0, recebo o maior elemento da esquerda
+            // comparo se o meu menor elemento é maior do que o maior elemento recebido (se sim, estou ordenado em relação ao meu vizinho)
+            // compartilho o meu estado com todos os processos
+            // se todos estiverem ordenados com seus vizinhos, a ordenação do vetor global está pronta ( pronto = TRUE, break)
+            if(my_rank > 0){   
+                //flag_erro = 0;
+                printf("♥♥♥ DEBUG recebido %d menor que %d ? \n", recebido, ini_vetor);
+                if(recebido < vetor[ini_vetor]) {
+                    // Está ordenado com o da esquerda, manda broadcast positivo
+                    //MPI_Bcast(1, 1, MPI_INT, tag_feedback, MPI_COMM_WORLD);
+                    
+                    for (i = 0; i < np; i++)
+                        if(i != my_rank)
+                            MPI_Send(1,1,MPI_INT,i,tag_feedback,MPI_COMM_WORLD);
+                } else {
+                    // Nao esta ordenado com o da esquerda, manda broadcast negativo
+                    //MPI_Bcast(2, 1, MPI_INT, tag_feedback, MPI_COMM_WORLD);
+                    for (i = 0; i < np; i++)
+                        if(i != my_rank)
+                            MPI_Send(2,1,MPI_INT,i,tag_feedback,MPI_COMM_WORLD);
+                    //flag_erro = 1;
+                }
+            }
+            if(debug == 1) printf("♥♥♥ DEBUG [ %d ] TAG_MAIOR-fim \n", my_rank);
+        }else if(status.MPI_TAG == tag_feedback){
+            if(debug == 1) printf("♥♥♥ DEBUG [ %d ] TAG_FEEDBACK \n", my_rank);
+            int positv = 0, negatv = 0;
+            if(recebido == 1) positv++;
+            else negatv++;
+
+            if(((negatv + positv) == np-1)){ // Se todas as mensagens CHEGARAM !
+                printf("♥♥♥ DEBUG p= %d recebeu todas mensagens  \n", ini_vetor);
+                if(negatv > 0 && my_rank != 0){
+                    // entao teve um erro
+                    // Troca pedaço do vetor com o vizinho
+                    int i_aux;
+                    int aux_lim_vizinho, aux_meu_lim;
+                    aux_meu_lim = ((ini_vetor) + (tam_part/10));
+                    aux_lim_vizinho = ((ini_vetor - 1) - (tam_part/10));
+                    int vet_aux[(tam_part/10)];
+                    // Faco minha copia
+                    for(i = ini_vetor; i < aux_meu_lim; i++)
+                        vet_aux[i] = vetor[i];
+                    // Pego os dados do vizinhos (my_rank - 1)
+                    i_aux = ini_vetor;
+                    for(i = aux_lim_vizinho; i < (ini_vetor); i++){
+                        vetor[i_aux] = vetor[i];
+                        i_aux++;
+                    }
+                    // Manda meus dados de Copia para o vizinho
+                    i_aux = aux_lim_vizinho;
+                    for(i = 0; i < (tam_part/10);i++){
+                        vetor[i_aux] = vet_aux[i];
+                        i_aux++;
+                    }
+                    // Avisa o vizinho que ele tem que ordenar novamente por que deu erro
+                    MPI_Send(1,1,MPI_INT,(my_rank -1), tag_erro, MPI_COMM_WORLD);
+                }else{
+                    // tudo certo pode terminar!
+                    pronto = 1;
+                    if(my_rank != 0){
+                        MPI_Finalize();
+                        return 0;
+                    }
+                }
+            }
+            if(debug == 1) printf("♥♥♥ DEBUG [ %d ] TAG_FEEDBACK-fim \n", my_rank);
+        }
+        
+      
+
+   // troco valores para convergir
+
+      // se não for o 0, mando os menores valores do meu vetor para a esquerda
+      // se não for np-1, recebo os menores valores da direita
+
+         // ordeno estes valores com a parte mais alta do meu vetor local
+
+         // devolvo os valores que recebi para a direita
+
+      // se não for o 0, recebo de volta os maiores valores da esquerda
+    }
+      
+
+    if(my_rank == 0){
+        // Fim do Tempo
+		tf = MPI_Wtime();
+		double total_time;
+		total_time = tf - ti;
+		printf("\n TEMPO TOTAL = %f \n", total_time);
+        MPI_Finalize();
+        return 0;
+    }
 
     MPI_Finalize();
     return 0;
